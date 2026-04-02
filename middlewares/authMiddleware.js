5dd4038966a82f8242afa276d1303d67061e96ef
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-04-02 21:27:19 +0530

    feat: add auth middleware with JWT verification and user role authorization

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,40 @@
 import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized." });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET);
+
+    req.user = prisma.user.findUnique({
+      where: { id: decoded.userId },
+      select: { id: true, email: true, role: true, status: true },
+    });
+
+    if (!user || user.status == "INACTIVE") {
+      return res
+        .status(401)
+        .json({ message: "User not authorized or account inactive." });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
+
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role ${req.user.role} is not authorized for this action.`,
+      });
+    }
+  };
+};
